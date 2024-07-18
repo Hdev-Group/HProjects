59be66007dcac09fb27a177abfa65140d50c24c5
CampbellHarry <hthebeast7@gmail.com>
2024-07-18 23:19:52 +0100

    404 page added

--- HProjects/src/app/not-found.js
+++ HProjects/src/app/not-found.js
@@ -1,8 +1,58 @@
+"use client";
+import HeaderIndex from '../components/header/header';
+import { usePathname } from 'next/navigation';
+
 export default function NotFound() {
+  const randomtext = () => {
+    const texts = ['Oops, Looks like we lost track of this page. But your projects are fine.', 'We found you! Get back to your projects.', 'Looks like you are lost, but your projects are safe.', 'This page is not found, but your projects are safe.', 'Looks like we lost track of this page. But your projects are fine.', 'Your lost, Get back to your projects.']
+    return texts[Math.floor(Math.random() * texts.length)]
+  }
+  const pathname = usePathname()
   return (
-    <div>
-      <h1>404 - Page Not Found</h1>
-      <p>Sorry, the page you are looking for does not exist.</p>
-    </div>
+    <>
+      <title>
+        404 - Page Not Found
+      </title>
+      <HeaderIndex />
+      <div className='h-full pt-40 px-2 md:px-20 relative w-full flex-row md:flex-row'>
+        <div className='flex flex-col md:flex-row w-full'>
+        <div className='w-full z-[100] relative md:w-[45%]  flex flex-col gap-10'>
+          <h1 className='font-bold text-5xl w-full boxtexts leading-snug'>{randomtext()}</h1>
+          <p className='text-xl'>Get back to your projects.</p>
+          <a href='/dashboard' className='font-semibold text-white p-2 rounded-3xl w-1/6 text-center bg-black border'>Dashboard</a>
+          <p className='text-xs'>Nerd stuff: 404 - Unable to find page: {pathname}</p>
+        </div>
+        <div className='flex relative pt-[50rem]  items-center  justify-center w-full md:w-[50%] z-[-10]'>
+          <div className='absolute left-[55rem] md:left-[60rem]'>
+          <div className='arm hidden md:flex bg-gray-700 h-80 rounded-full w-20 absolute rotate-[50deg] bottom-[20rem] right-[59rem]'></div>
+          <div className='arm1 bg-gray-700 h-80 rounded-full w-20 absolute rotate-[-50deg] bottom-[20rem] right-[10rem]'></div>
+          <div className='clipboard bg-neutral-600 rounded-2xl p-5 h-[30rem] w-[40rem] absolute bottom-[15rem] right-[17rem]'>
+            <div>
+              <div className='bg-neutral-500 h-10 w-30 rounded-2xl'></div>
+            </div>
+            <div className='flex flex-col mt-10 gap-5 h-1/2 justify-between'>
+              <div className='gap-4 flex flex-col'>
+                <div className='bg-neutral-500 h-5 w-20 rounded-2xl'></div>
+                <div className='bg-neutral-500 h-5 w-50 rounded-2xl'></div>
+              </div>
+              <div className='gap-4 flex flex-col'>
+                <div className='bg-neutral-500 h-5 w-20 rounded-2xl'></div>
+                <div className='bg-neutral-500 h-5 w-50 rounded-2xl'></div>
+              </div>
+              <div className='gap-4 flex flex-col'>
+                <div className='bg-neutral-500 h-5 w-20 rounded-2xl'></div>
+                <div className='bg-neutral-500 h-5 w-50 rounded-2xl'></div>
+              </div>
+              <div className='gap-4 flex flex-col'>
+                <div className='bg-neutral-500 h-5 w-20 rounded-2xl'></div>
+                <div className='bg-neutral-500 h-5 w-50 rounded-2xl'></div>
+              </div>
+            </div>
+          </div>
+          </div>
+        </div>
+        </div>
+      </div>
+    </>
   );
 }
