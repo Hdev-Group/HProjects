b9743b99ff79480a7120a11cdd49ef082dee6891
CampbellHarry <hthebeast7@gmail.com>
2024-09-12 16:34:32 +0100

    update

--- src/service/aws-ses.js
+++ src/service/aws-ses.js
@@ -2,7 +2,7 @@ import AWS from 'aws-sdk';
 
 AWS.config.update({ 
   region: 'eu-north-1',  
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID, 
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY, 
 });
 
